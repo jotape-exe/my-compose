6a29011d1b1b79eb79793e3f02f8b45ebb48953e
jotape-exe <joaoxstone@gmail.com>
2024-03-19 22:46:57 -0300

    state and animations

--- app/src/main/java/com/joaoxstone/jxscompose/MainActivity.kt
+++ app/src/main/java/com/joaoxstone/jxscompose/MainActivity.kt
@@ -3,7 +3,13 @@ package com.joaoxstone.jxscompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,17 +18,25 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -37,9 +51,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    LazyColumn(modifier = Modifier
-                        .padding(12.dp)
-                        .fillMaxSize()) {
+                    LazyColumn(
+                        modifier = Modifier
+                            .padding(12.dp)
+                            .fillMaxSize()
+                    ) {
                         items(lista) { item ->
                             JXSCard(title = item.title)
                         }
@@ -67,21 +83,54 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     fun JXSCard(title: String) {
-        Card(modifier = Modifier.padding(4.dp)) {
-            Row(
+        //rememberSaveable usado em mudanças de config: Tema, Orientação de tela...
+        var isExpanded by rememberSaveable { mutableStateOf(false) }
+        val text =
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+        Card(
+            modifier = Modifier
+                .padding(4.dp)
+        ) {
+            Column(
                 modifier = Modifier
-                    .padding(8.dp)
-                    .fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween
+                    .clickable {
+                        isExpanded = !isExpanded
+                    }
+                    .animateContentSize(
+                        animationSpec = spring(
+                            dampingRatio = Spring.DampingRatioMediumBouncy,
+                            stiffness = Spring.StiffnessLow
+                        )
+                    )
             ) {
-                Text(text = title, fontSize = 20.sp, modifier = Modifier.padding(end = 8.dp))
-                JXSButton()
+                Row(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(text = title, fontSize = 20.sp, modifier = Modifier.padding(end = 8.dp))
+                    IconButton(onClick = { isExpanded = !isExpanded }) {
+                        Icon(
+                            Icons.Default.KeyboardArrowDown,
+                            contentDescription = "",
+                            modifier = Modifier.rotate(if (isExpanded) 180f else 0f)
+                        )
+                    }
+                }
+                if (isExpanded) {
+                    Text(text = text, modifier = Modifier.padding(8.dp))
+                }
+
+
             }
+
         }
+
     }
-
-
-    data class CardData(val title: String)
 }
+
+
+data class CardData(val title: String)
 
