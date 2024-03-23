af8bb748a00617a98636a09998c31fb7c31d43b8
jotape-exe <joaoxstone@gmail.com>
2024-03-23 09:04:52 -0300

    compose layouts codelab

--- app/src/main/java/com/joaoxstone/jxscompose/CodelabCompose.kt
+++ app/src/main/java/com/joaoxstone/jxscompose/CodelabCompose.kt
@@ -8,6 +8,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.material3.windowsizeclass.WindowSizeClass
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -16,21 +20,33 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.paddingFromBaseline
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationRail
+import androidx.compose.material3.NavigationRailItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -41,53 +57,58 @@ import androidx.compose.ui.unit.dp
 import com.example.compose.JXSComposeTheme
 
 class CodelabCompose : ComponentActivity() {
+
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val lista = listOf(
-                RowItemData("row 1", R.drawable.row_1),
-                RowItemData("row 3", R.drawable.row_3),
-                RowItemData("row 4", R.drawable.row_4),
-                RowItemData("row 5", R.drawable.row_5),
-                RowItemData("row 6", R.drawable.row_6),
-                RowItemData("row 7", R.drawable.row_7),
-                RowItemData("row 8", R.drawable.row_8)
-            )
-            JXSComposeTheme {
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                ) {
-                    SearchBar(modifier = Modifier.padding(8.dp))
-                    LazyRow(
-                        contentPadding = PaddingValues(horizontal = 16.dp),
-                        modifier = Modifier
-                            .fillMaxWidth(),
-                        horizontalArrangement = Arrangement.spacedBy(8.dp)
-                    ) {
-                        items(lista) {
-                            RowItem(imageId = it.imageId, description = it.description)
-                        }
-                    }
-
-                    LazyHorizontalGrid(
-                        rows = GridCells.Fixed(2),
-                        contentPadding = PaddingValues(horizontal = 16.dp),
-                        horizontalArrangement = Arrangement.spacedBy(16.dp),
-                        verticalArrangement = Arrangement.spacedBy(16.dp),
-                        modifier = Modifier.height(168.dp)
-                    ) {
-                        items(lista) {
-                            FavoriteCollectionCard(
-                                imageId = it.imageId,
-                                description = it.description,
-                                modifier = Modifier.height(100.dp)
-                            )
-                        }
-                    }
-                }
-            }
+            val windowSizeClass = calculateWindowSizeClass(this)
+            MySootheApp(windowSizeClass)
         }
+    }
+}
+
+
+@Composable
+fun HomeScreen(lista: List<RowItemData>){
+    Spacer(Modifier.height(16.dp))
+    SearchBar(modifier = Modifier.padding(horizontal = 16.dp))
+    HomeSection {
+        RowList(lista)
+    }
+    GridSection {
+        GridList(lista)
+    }
+    Spacer(Modifier.height(16.dp))
+}
+
+@Composable
+fun HomeSection(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Column(modifier) {
+        Text(
+            "Slot Horizontal", style = MaterialTheme.typography.titleMedium, modifier = Modifier
+                .paddingFromBaseline(top = 40.dp, bottom = 16.dp)
+                .padding(horizontal = 16.dp)
+        )
+        content()
+    }
+}
+
+@Composable
+fun GridSection(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Column(modifier) {
+        Text(
+            "Slot HorizontalGrid", style = MaterialTheme.typography.titleMedium, modifier = Modifier
+                .paddingFromBaseline(top = 40.dp, bottom = 16.dp)
+                .padding(horizontal = 16.dp)
+        )
+        content()
     }
 }
 
@@ -138,6 +159,169 @@ fun RowItem(imageId: Int, description: String, modifier: Modifier = Modifier) {
     }
 }
 
+@Composable
+fun RowList(lista: List<RowItemData>) {
+    LazyRow(
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = Modifier
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(lista) {
+            RowItem(imageId = it.imageId, description = it.description)
+        }
+    }
+}
+
+@Composable
+fun GridList(lista: List<RowItemData>) {
+    LazyHorizontalGrid(
+        rows = GridCells.Fixed(2),
+        contentPadding = PaddingValues(horizontal = 8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = Modifier.height(168.dp)
+    ) {
+        items(lista) {
+            FavoriteCollectionCard(
+                imageId = it.imageId,
+                description = it.description,
+                modifier = Modifier.height(100.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun SootheBottomNavigation(modifier: Modifier = Modifier) {
+    NavigationBar(
+        containerColor = MaterialTheme.colorScheme.surfaceVariant,
+        modifier = modifier
+    ) {
+        NavigationBarItem(
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Home,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text("Home")
+            },
+            selected = true,
+            onClick = {}
+        )
+        NavigationBarItem(
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Email,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text("Mail")
+            },
+            selected = false,
+            onClick = {}
+        )
+        NavigationBarItem(
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.ShoppingCart,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text("Shop")
+            },
+            selected = false,
+            onClick = {}
+        )
+        NavigationBarItem(
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.AccountCircle,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text("Profile")
+            },
+            selected = false,
+            onClick = {}
+        )
+    }
+}
+
+@Composable
+private fun SootheNavigationRail(modifier: Modifier = Modifier) {
+    NavigationRail(
+        modifier = modifier.padding(horizontal = 8.dp),
+        containerColor = MaterialTheme.colorScheme.background,
+    ) {
+        Column(
+            modifier = Modifier.fillMaxHeight(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            NavigationRailItem(
+                icon = {
+                    Icon(
+                        imageVector = Icons.Default.Home,
+                        contentDescription = null
+                    )
+                },
+                label = {
+                    Text("Home")
+                },
+                selected = true,
+                onClick = {}
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            NavigationRailItem(
+                icon = {
+                    Icon(
+                        imageVector = Icons.Default.Email,
+                        contentDescription = null
+                    )
+                },
+                label = {
+                    Text("Mail")
+                },
+                selected = false,
+                onClick = {}
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            NavigationRailItem(
+                icon = {
+                    Icon(
+                        imageVector = Icons.Default.ShoppingCart,
+                        contentDescription = null
+                    )
+                },
+                label = {
+                    Text("Shop")
+                },
+                selected = false,
+                onClick = {}
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            NavigationRailItem(
+                icon = {
+                    Icon(
+                        imageVector = Icons.Default.AccountCircle,
+                        contentDescription = null
+                    )
+                },
+                label = {
+                    Text("Profile")
+                },
+                selected = false,
+                onClick = {}
+            )
+        }
+    }
+}
 
 @Composable
 fun FavoriteCollectionCard(modifier: Modifier = Modifier, description: String, imageId: Int) {
@@ -162,5 +346,51 @@ fun FavoriteCollectionCard(modifier: Modifier = Modifier, description: String, i
     }
 }
 
+@Composable
+fun MySootheAppLandscape(lista: List<RowItemData>) {
+    JXSComposeTheme {
+        Surface(color = MaterialTheme.colorScheme.background) {
+            Row {
+                SootheNavigationRail()
+                HomeScreen(lista)
+            }
+        }
+    }
+}
 
+@Composable
+fun MySootheAppPortrait(lista: List<RowItemData>){
+    JXSComposeTheme {
+        Scaffold(bottomBar = { SootheBottomNavigation() }) {
+            Column(
+                modifier = Modifier
+                    .padding(it)
+                    .fillMaxSize()
+                    .verticalScroll(rememberScrollState())
+            ) {
+                HomeScreen(lista)
+            }
+        }
+    }
+}
+@Composable
+fun MySootheApp(windowSize: WindowSizeClass) {
+    val lista = listOf(
+        RowItemData("row 1", R.drawable.row_1),
+        RowItemData("row 3", R.drawable.row_3),
+        RowItemData("row 4", R.drawable.row_4),
+        RowItemData("row 5", R.drawable.row_5),
+        RowItemData("row 6", R.drawable.row_6),
+        RowItemData("row 7", R.drawable.row_7),
+        RowItemData("row 8", R.drawable.row_8)
+    )
+    when (windowSize.widthSizeClass) {
+        WindowWidthSizeClass.Compact -> {
+            MySootheAppPortrait(lista)
+        }
+        WindowWidthSizeClass.Expanded -> {
+            MySootheAppLandscape(lista)
+        }
+    }
+}
 data class RowItemData(val description: String, val imageId: Int)
